498f056775b518717794a47ff88f050c43d1bc52
HugoLeander <hugo.leander@live.se>
2024-03-09 11:25:05 +0100

    Can now see all venues at /venues

--- Web-app/Presentation/Routers/homepage-router.js
+++ Web-app/Presentation/Routers/homepage-router.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const Venue = require('../../script')
 
 
 module.exports = function({}){
@@ -8,6 +9,16 @@ module.exports = function({}){
 		console.log("är du här")
 		response.render("homepage.hbs")
 	})
+
+	router.get("/venues", async (request, response) => {
+		try {
+			const venues = await Venue.fetchAllVenues();
+			response.render("venues", { venues }); // Pass the venues to the Handlebars template
+		} catch (error) {
+			console.error('Error fetching venues:', error);
+			response.status(500).send('Error fetching venues');
+		}
+	});
 
 	router.get("/about", function(request, response){
 		response.render("about.hbs")
