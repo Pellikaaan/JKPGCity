6fa73a8d52189101bda94e2af447ee4b835003a1
Pellikaaan <98748085+Pellikaaan@users.noreply.github.com>
2024-03-17 12:10:47 +0100

    fixed update fucntion so that it also displays on the web

--- Web-app/Presentation/Routers/venues-router.js
+++ Web-app/Presentation/Routers/venues-router.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const Venue = require('../../venuesService');
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
-module.exports = function({}){
+
+module.exports = function ({ }) {
     const router = express.Router();
+    router.use(bodyParser.json())
+    router.use(express.urlencoded({
+        extended: false,
+    }))
 
     router.get('/', async (request, response) => {
+        console.log("lukta snutt")
         const { districts, sort, order } = request.query;
         let filterOptions = {};
         let sortOptions = {};
@@ -35,5 +43,54 @@ module.exports = function({}){
         }
     });
 
-    return router;
+    router.post('/delete', async (request, response) => {
+        console.log("lukta ännu mer snutt")
+        try {
+          const { name, url, district,rating } = request.body;
+          // Remove the venue from MongoDB using Mongoose
+          await Venue.deleteVenue(name,url,district,rating);
+          //response.sendStatus(200); Send a success response
+          response.redirect('/venues')
+        } catch (error) {
+          console.error('Error removing venue:', error);
+          //response.status(500).send('Internal Server Error'); // Send an error response
+          response.redirect('/venues')
+        }
+    });
+
+    router.get('/update/:_id', async (request, response) => {
+        console.log("Received _id:", request.params._id); // Debugging
+        const venue = await Venue.getVenueByID(request.params._id);
+        response.render('venues-update.hbs',venue)
+    });
+      
+
+    router.post('/update/:_id', async (request, response) => {
+        try {
+            const {_id} = await Venue.getVenueByID(request.params._id)
+          const {name,url,district,rating } = request.body;
+
+          const document = {
+            _id,
+            name,
+            url,
+            district,
+            rating
+        };
+
+         
+         
+         await Venue.updateVenue(document);
+         
+
+
+          // Render the template with the updated venue data
+          response.redirect('/venues');
+        } catch (error) {
+          console.error('Error removing venue:', error);
+          //response.status(500).send('Internal Server Error'); // Send an error response
+          response.redirect('/venues')
+        }
+    });
+return router;
 };
